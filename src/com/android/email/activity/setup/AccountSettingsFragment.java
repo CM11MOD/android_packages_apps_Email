1b0112a306745b9860303706b22422bba10f25fc
Lars Greiss <kufikugel@googlemail.com>
2014-07-02 22:01:28 +0200

    Email: Add native heads up support per account (1/2)
    Add native heads up notification support. User can set on every account
    if he or she want this account as heads up notification or not.
    
    This patch adds the settings for it as well the complete handling. Due
    of a half broken NotificationCompat.Builder class we switch here to the
    Notification.Builder class to get access to the methods we need. This is no
    problem here due that we do not need compatibility on lower API's
    
    Change-Id: Ia88d2f15f0eaf7ae024ab07dd4f74c6858f17f76

--- src/com/android/email/activity/setup/AccountSettingsFragment.java
+++ src/com/android/email/activity/setup/AccountSettingsFragment.java
@@ -133,6 +133,7 @@ public class AccountSettingsFragment extends PreferenceFragment
     private CheckBoxPreference mAccountBackgroundAttachments;
     private CheckBoxPreference mInboxNotify;
     private CheckBoxPreference mInboxVibrate;
+    private CheckBoxPreference mInboxHeadsUp;
     private CheckBoxPreference mInboxNotifyEveryMessage;
     private Preference mInboxRingtone;
     private PreferenceCategory mNotificationsCategory;
@@ -416,6 +417,12 @@ public class AccountSettingsFragment extends PreferenceFragment
             mInboxFolderPreferences.setNotificationVibrateEnabled(vibrateSetting);
             preferenceChanged(FolderPreferences.PreferenceKeys.NOTIFICATION_VIBRATE, newValue);
             return true;
+        } else if (FolderPreferences.PreferenceKeys.NOTIFICATION_HEADS_UP.equals(key)) {
+            final boolean headsUpSetting = (Boolean) newValue;
+            mInboxHeadsUp.setChecked(headsUpSetting);
+            mInboxFolderPreferences.setNotificationHeadsUpEnabled(headsUpSetting);
+            preferenceChanged(FolderPreferences.PreferenceKeys.NOTIFICATION_HEADS_UP, newValue);
+            return true;
         } else if (FolderPreferences.PreferenceKeys.NOTIFICATION_NOTIFY_EVERY_MESSAGE.equals(key)) {
             final boolean notifyEveryMessageSetting = (Boolean) newValue;
             mInboxNotifyEveryMessage.setChecked(notifyEveryMessageSetting);
@@ -664,6 +671,8 @@ public class AccountSettingsFragment extends PreferenceFragment
                                     mInboxFolderPreferences.areNotificationsEnabled());
                             mInboxVibrate.setChecked(
                                     mInboxFolderPreferences.isNotificationVibrateEnabled());
+                            mInboxHeadsUp.setChecked(
+                                    mInboxFolderPreferences.isNotificationHeadsUpEnabled());
                             mInboxNotifyEveryMessage.setChecked(
                                     mInboxFolderPreferences.isEveryMessageNotificationEnabled());
                             setRingtoneSummary();
@@ -851,6 +860,10 @@ public class AccountSettingsFragment extends PreferenceFragment
 
         mNotificationsCategory =
                 (PreferenceCategory) findPreference(PREFERENCE_CATEGORY_NOTIFICATIONS);
+
+        mInboxHeadsUp = (CheckBoxPreference) findPreference(
+                FolderPreferences.PreferenceKeys.NOTIFICATION_HEADS_UP);
+        mInboxHeadsUp.setOnPreferenceChangeListener(this);
 
         // Set the vibrator value, or hide it on devices w/o a vibrator
         mInboxVibrate = (CheckBoxPreference) findPreference(
